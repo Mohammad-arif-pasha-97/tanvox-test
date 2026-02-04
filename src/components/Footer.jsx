25b106ca90152427625509dcddb261b664327ff7
Mohammad Arif Pasha <154226365+Mohammad-arif-pasha-97@users.noreply.github.com>
2026-02-04 11:13:53 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-10 text-center bg-[#2B1B12] text-[#F5EFE6]">
-      <p>© 2026 Tanvox Technology. All rights reserved.</p>
-    </footer>
-  );
-}
